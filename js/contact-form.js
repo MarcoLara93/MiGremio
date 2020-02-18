0c9ef22191cc1a226669239fa9ec95c6e270f279
guido2d <guido.disalvoo@gmail.com>
2020-02-18 15:54:16 -0300

    texto contacto

--- js/contact-form.js
+++ js/contact-form.js
@@ -26,31 +26,9 @@
 
             console.log(data);
 
-            // handle errors
-            if (!data.success) {
-                if (data.errors.name) {
-                    $('#name-field').addClass('has-error');
-                    $('#name-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.name + '</span>');
-                }
+            // display success message
+            $form.html('<div class="alert alert-success">' + data + '</div>');
 
-                if (data.errors.email) {
-                    $('#email-field').addClass('has-error');
-                    $('#email-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.email + '</span>');
-                }
-
-                if (data.errors.subject) {
-                    $('#subject-field').addClass('has-error');
-                    $('#subject-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.subject + '</span>');
-                }
-
-                if (data.errors.message) {
-                    $('#message-field').addClass('has-error');
-                    $('#message-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.message + '</span>');
-                }
-            } else {
-                // display success message
-                $form.html('<div class="alert alert-success">' + data.message + '</div>');
-            }
         }).fail(function (data) {
             // for debug
             console.log(data)
